69007c51d2c54375d19bcf2f51da6542a7d5cdd8
Neil <62321203+CestMerNeil@users.noreply.github.com>
2023-01-31 11:59:16 +0100

    Update Group and Player

--- src/Group.cpp
+++ src/Group.cpp
@@ -1,1 +1,15 @@
 #include "Group.hpp"
+
+Group::Group(){}
+Group::Group(int id){
+    this->_id = id;
+    this-> _name = "default";
+    this-> _num_player = NUM_PLAYER;
+}
+
+// With overwrite, we can create a team with his name.
+Group::Group(int id, std::string name){
+    this->_id = id;
+    this->_name = name;
+    this->_num_player = NUM_PLAYER;
+}
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,41 +1,46 @@
 #include "Player.hpp"
 
-Player::Player(Group g)
-{
+Player::Player(Group g){
     this->_sunshine = 0;
     this->_object = NULL;
     this->_group = g;
-
+    this->_direction = Up; // Direction up by default
+    this->_speed = 0;
 }
 
-void Player::setSunshine(int numOfsunshine)
-{
+void Player::setSunshine(int numOfsunshine){
     this->_sunshine = numOfsunshine;
 }
 
-int Player::getSunshine()
-{
+int Player::getSunshine(){
     return _sunshine;
 }
 
-void Player::setObject(Object* object)
-{
+void Player::setObject(Object * object){
     this->_object = object;
 }
 
-Object* Player::getObject()
-{
+Object * Player::getObject(){
     return _object;
 }
 
-void Player::seDeplacer()
-{}
+void Player::setDirection(Direction direction){
+    this->_direction = direction;
+}
 
-void Player::useObject()
-{
+Direction Player::getDirection(){
+    return _direction;
+}
+
+void Player::seDeplacer(){
+    
+}
+
+void Player::useObject(){
     // function of this object;
     this->_object = NULL;
 }
 
 void Player::makeDecision()
 {}
+
