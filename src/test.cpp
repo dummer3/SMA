787e23303284bf236f9cb687acafd5ccdda9d82a
Matthias Benquet <matthiasbenquet@gmail.com>
2023-02-27 16:57:22 +0100

    oubli de fichiers du dernier commit

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <iostream>
-#include "Map.hpp"
-#include "catch.hpp"
+#include "../includes/Map.hpp"
+#include "../includes/catch.hpp"
 #include <sstream>
 #include <unistd.h>
 
@@ -31,33 +31,6 @@ TEST_CASE("Unit_Tests") {
 }
 
 /*********** Integration Tests ***********/
-TEST_CASE("Integration_Tests") {
-    unsigned int frame = 50;
-    int argc = 2; // It`s just a demo for the test.
-    char * argv; // No idea with the input for this param. !!!
 
-    if (argc > 1) {
-        std::istringstream ss(argv[1]);
-        if (!(ss >> frame)) {
-            std::cerr << "Invalid number: " << argv[1] << '\n';
-        } else if (!ss.eof()) {
-            std::cerr << "Trailing characters after number: " << argv[1] << '\n';
-        }
-    }
-
-    frame *= 10000;
-    srand(time(0));
-
-    GameController *g = GameController::Get();
-    g->InitGame();
-
-    for (int i = 0; i < 100; i++) {
-        g->NewRound();
-        usleep(frame);
-    }
-
-    g->Destroy();
-    // delete g;
-}
 /*********** System Tests ***********/
 TEST_CASE("System_Tests") {}
