1750fdca3b4a856c73ce14a4ebff18ec06c3e150
Neil <62321203+CestMerNeil@users.noreply.github.com>
2023-04-17 04:45:55 +0900

    Revert "Merge pull request #1 from dummer3/TEST_PART"
    This reverts commit 04e6740522a2bcfc3649e583300ad27aabe9a28c, reversing
    changes made to bcdb6b8315a1e80d55721358d8fce99f05807556.

--- src/test.cpp
+++ src/test.cpp
@@ -1,83 +1,31 @@
 #define CATCH_CONFIG_MAIN
-
+#include "../includes/Map.hpp"
 #include "../includes/catch.hpp"
-#include "../src/Box.cpp"
-#include "../src/GameController.cpp"
-#include "../src/Group.cpp"
-#include "../src/Iplaceable.cpp"
-#include "../src/Map.cpp"
-#include "../src/Object.cpp"
-#include "../src/Player.cpp"
-#include "../src/Sun.cpp"
-
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
 
-TEST_CASE("First Test")
-{
-  std::cout << "Hello World" << std::endl;
-  int x = 1;
-  REQUIRE(x == 1);
-}
+TEST_CASE("First Test") { std::cout << "Hello World" << std::endl; }
 
 /*********** Unit Tests ***********/
+TEST_CASE("Unit_Tests") {
 
-TEST_CASE("Unit Test of BOX", "[Box]")
-{
-  Box box(2, 3);
-  Player *player;
-  REQUIRE(box.Manage() == true);
-  box.generateContent();
-  REQUIRE(box.Manage() == false);
-  REQUIRE(box.getX() == 3);
-  REQUIRE(box.getY() == 2);
+  SECTION("Test_of_Box") {}
+  SECTION("Test_of_GameController") {}
+  SECTION("Test_of_Group") {}
+  SECTION("Test_of_Interface-Lace-able") {
+    // This is a test for an "interface"
+    // So this class cannot be instantiated
+  }
+  SECTION("Test_of_Map") {}
+  SECTION("Test_of_Object") {
+    // Juste this class can be instantiated.
+  }
+  SECTION("Test_of_Player") {}
+  SECTION("Test_of_Sun") {}
 }
 
-TEST_CASE("Unit Test of GameController", "[GameController]")
-{
-  GameController *game = GameController::Get();
-  REQUIRE(game != nullptr);
-  // REQUIRE(game->EvaluateTile(10, 10) == 10);
-  GameController *game1 = GameController::Get();
-  GameController *game2 = GameController::Get();
-  REQUIRE(game1 == game2);
-}
+/*********** Integration Tests ***********/
 
-TEST_CASE("Unit Test of Player", "[Player]")
-{
-  class TestPlayer : public Player
-  {
-  };
-  TestPlayer *p;
-  REQUIRE(p != nullptr);
-}
-
-TEST_CASE("Unit Test of Group", "[Group]")
-{
-
-  Group *group;
-  Player *player;
-
-  REQUIRE(group != nullptr);
-  REQUIRE(player != nullptr);
-
-  //(*group).AddPlayer(player);
-  REQUIRE((*group).OnMyGroup(player) == false);
-
-  Group g1(1, "My Group");
-  Group g2(2, "Other Group");
-
-  REQUIRE(g1.SameGroup(&g1) == true);
-  REQUIRE(g1.SameGroup(&g2) == false);
-}
-
-/* TEST_CASE("Unit Test of Object", "[Object]")
-{
-  Group g;
-  Player *p;
-  p->setDirection(Up);
-  Banana banana;
-  banana.hitEffect(p);
-  REQUIRE(p->getBoostTimer() == 4);
-} */
+/*********** System Tests ***********/
+TEST_CASE("System_Tests") {}
